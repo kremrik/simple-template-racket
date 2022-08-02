f4d859741e5f7f505124d4599d12f56b074b21a0
kremrik <kyle.robert.e@gmail.com>
2022-08-01 21:24:14 -0500

    backing up rando testing

--- main.rkt
+++ main.rkt
@@ -1,1 +1,24 @@
 #lang racket
+
+(require "template.rkt")
+
+
+(define (is-var in)
+  (let ([pad (make-string 5 #\space)])
+    (for ([line (in-lines in)])
+      (display
+       (string-append
+        (format "~a" (contains-var? line))
+        pad
+        line))
+      (newline))))
+
+;(call-with-input-file "index.html"
+;  (lambda (in)
+;    (is-var in)))
+
+
+(define (indent-lines in spaces)
+  (for ([line (in-lines in)])
+    (display (indent-line line spaces))
+    (newline)))
